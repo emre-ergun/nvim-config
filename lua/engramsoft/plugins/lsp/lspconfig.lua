4d9b4ff22443cab8b3e0ca4a88173394c1a9776a
Emre Ergun <emreergun@engramsoft.com>
2025-09-06 15:59:05 +0300

    sign_define function replaced with up-to-date version. it was deprecated

--- lua/engramsoft/plugins/lsp/lspconfig.lua
+++ lua/engramsoft/plugins/lsp/lspconfig.lua
@@ -69,11 +69,16 @@ return {
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.HINT] = "󰠠 ",
+					[vim.diagnostic.severity.INFO] = " ",
+				},
+			},
+		})
 
 		-- ✅ mason-lspconfig v2: setup() + handlers inside
 		mason_lspconfig.setup({
