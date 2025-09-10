a3e42d10a0c3a2121ffd75bb77ddf54316a76482
Emre Ergun <emreergun@engramsoft.com>
2025-09-10 12:32:54 +0300

    shortcuts added to change theme with keys

--- lua/engramsoft/plugins/colorscheme.lua
+++ lua/engramsoft/plugins/colorscheme.lua
@@ -41,5 +41,79 @@ return {
 		})
 
 		vim.cmd("colorscheme tokyonight")
+
+		---------------------------------------------------------------------
+		-- List of all installed colorschemes (builtin + plugins)
+		---------------------------------------------------------------------
+		local schemes = {
+			"blue",
+			"darkblue",
+			"default",
+			"delek",
+			"desert",
+			"elflord",
+			"evening",
+			"habamax",
+			"industry",
+			"koehler",
+			"lunaperche",
+			"morning",
+			"murphy",
+			"pablo",
+			"peachpuff",
+			"quiet",
+			"retrobox",
+			"ron",
+			"rose-pine",
+			"rose-pine-dawn",
+			"rose-pine-main",
+			"rose-pine-moon",
+			"shine",
+			"slate",
+			"sorbet",
+			"tokyonight",
+			"tokyonight-day",
+			"tokyonight-moon",
+			"tokyonight-night",
+			"tokyonight-storm",
+			"torte",
+			"unokai",
+			"vim",
+			"wildcharm",
+			"zaibatsu",
+			"zellner",
+		}
+
+		local idx = 1
+
+		---------------------------------------------------------------------
+		-- Apply a colorscheme safely
+		---------------------------------------------------------------------
+		local function apply_scheme(i)
+			idx = ((i - 1) % #schemes) + 1
+			local ok = pcall(vim.cmd.colorscheme, schemes[idx])
+			if ok then
+				vim.notify("Colorscheme → " .. schemes[idx], vim.log.levels.INFO)
+			else
+				vim.notify("Failed to load: " .. schemes[idx], vim.log.levels.ERROR)
+			end
+		end
+
+		---------------------------------------------------------------------
+		-- Keymaps
+		---------------------------------------------------------------------
+		-- Cycle next/previous
+		vim.keymap.set("n", "<leader>cn", function()
+			apply_scheme(idx + 1)
+		end, { desc = "Next colorscheme" })
+		vim.keymap.set("n", "<leader>cp", function()
+			apply_scheme(idx - 1)
+		end, { desc = "Prev colorscheme" })
+
+		-- Reset to Tokyonight (your default)
+		vim.keymap.set("n", "<leader>cr", function()
+			vim.cmd("colorscheme tokyonight")
+			vim.notify("Colorscheme → tokyonight (reset)", vim.log.levels.INFO)
+		end, { desc = "Reset to Tokyonight" })
 	end,
 }
