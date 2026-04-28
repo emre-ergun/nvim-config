15b6375659e5aab6d0e331c3737e37b2c3be26b2
Emre Ergun <emreergun@engramsoft.com>
2026-04-28 16:56:40 +0300

    configration from Phaeno added

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
-require("engramsoft.core")
-require("engramsoft.lazy")
-require("engramsoft.lsp")
+-- vim ui2
+-- require('vim._core.ui2').enable({
+--     enable = true,
+--     msg = {
+--         target = "cmd", -- options: cmd(classic), msg(similar to noice)
+--         pager = { height = 1 },
+--         msg   = { height = 0.5, timeout = 4500 },
+--         dialog = { height = 0.5 },
+--         cmd    = { height = 0.5 },
+--     },
+-- })
+
+require("Engram.core")
+require("Engram.lazy")
+require("current-theme")
