303303281e5bfd91ead347a34b9af4ec38ea9e1f
Emre Ergun <emreergun@engramsoft.com>
2026-04-30 14:33:59 +0300

    fix for deprecated usage removed

--- init.lua
+++ init.lua
@@ -1,10 +1,5 @@
 vim.opt.runtimepath:append("/Users/emre/.local/share/nvim/site")
 
--- fix the deprecated get_active_clients
-if not vim.lsp.get_active_clients then
-	vim.lsp.get_active_clients = vim.lsp.get_clients
-end
-
 require("engram.core")
 require("engram.lazy")
 require("current-theme")
